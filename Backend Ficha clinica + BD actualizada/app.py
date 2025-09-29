2628c85047bb455036effdcb3a1729c7aef199cc
simoncanaless <canalessimon06@gmail.com>
2025-09-28 21:43:47 -0300

    Update app.py

--- Backend Ficha clinica + BD actualizada/app.py
+++ Backend Ficha clinica + BD actualizada/app.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from flasgger import Swagger
 from db import get_connection
 from pymysql.cursors import DictCursor
+import os
+
 
 app = Flask(__name__)
 CORS(app)
@@ -1793,10 +1795,9 @@ def get_ficha_completa(rut):
 # MAIN
 # ============================================================
 
-import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
