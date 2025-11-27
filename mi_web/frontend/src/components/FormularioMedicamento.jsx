c0f8671e77e7e9383a9b389d5e9ce36815f19b86
Rodrigo <107264333+Rodrigo189@users.noreply.github.com>
2025-11-26 21:03:56 -0300

    Ahora no muestra el cargo del funcionario al modificar un medicamento asociado a un Residente

--- mi_web/frontend/src/components/FormularioMedicamento.jsx
+++ mi_web/frontend/src/components/FormularioMedicamento.jsx
@@ -74,7 +74,7 @@ export default function FormularioMedicamento({ medicamento, setEditing, refresh
           <option value="">Seleccione un funcionario</option>
           {funcionarios.map((f, index) => (
             <option key={index} value={`${f.nombres} ${f.apellidos}`}>
-              {f.nombres} {f.apellidos} - {f.cargo}
+              {f.nombres} {f.apellidos}
             </option>
           ))}
         </select>
