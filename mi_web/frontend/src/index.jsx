989055fcad834f37001f333ed5d50da219c1f97a
Rodrigo <107264333+Rodrigo189@users.noreply.github.com>
2025-12-01 13:20:53 -0300

    Arreglo de errores (creo)

--- mi_web/frontend/src/index.jsx
+++ mi_web/frontend/src/index.jsx
@@ -14,13 +14,30 @@ import MenuPrincipal from "./components/MenuPrincipal";
 import SeleccionUsuario from "./components/SeleccionUsuario";
 import InfoMedicamentos from "./components/InfoMedicamentos";
 import FichaClinica from "./pages/FichaClinica";
+import LoginSelectionLyP from "./components/LoginSelectPagos";
+import LoginSelectionRyE from "./components/LoginSelectReport";
+import LoginPyLFuncionario from "./components/LoginPagosFuncionario";
+import LoginPyLAdministrador from "./components/LoginPagosAdmin";
+import PerfilFuncionario from "./components/FuncionarioProfile";
+import PerfilAdministrador from "./components/AdminProfile";
+import ListaFuncionario from "./components/FuncionariosList";
+import SubPerfilFuncionario from "./components/SubFuncionario";
+import LoginRyEFuncionario from "./components/LoginReportFuncionario";
+import LoginRyEAdministrador from "./components/LoginReportAdmin";
+import FuncionarioDashboard from "./components/FuncionarioDashboard";
+import Estadisticas from "./components/EstadisticasPage";
+import Probabilidades from "./components/ProbabilidadesPage";
+import AnalisisRiesgo from "./components/RiesgosPage";
+import EstadisticasSistema from "./components/SistemaPage";
+import AdministradorDashboard from "./components/AdminDashboard";
+import ListaFuncionarioRyE from "./components/AdminFuncionariosList";
+
 import "@fontsource/inria-sans";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route
         <Route path="/" element={<MenuPrincipal />} />
         <Route path="/principal" element={<Principal />} />
         <Route path="/login" element={<LoginFuncionario />} />
@@ -33,6 +50,23 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/formulario-signos-vitales/:rut" element={<FormularioSignosVitales />} />
         <Route path="/seleccion-usuario" element={<SeleccionUsuario />} />
         <Route path="/medicamentos/:rut" element={<InfoMedicamentos />} />
+        <Route path="/LoginSelectionLyP" element={<LoginSelectionLyP />} />
+        <Route path="/LoginSelectionRyE" element={<LoginSelectionRyE />} />
+        <Route path="/FuncionarioDashboard" element={<FuncionarioDashboard />} />
+        <Route path="/LoginPyLFuncionario" element={<LoginPyLFuncionario />} />
+        <Route path="/LoginPyLAdministrador" element={<LoginPyLAdministrador />} />
+        <Route path="/LoginRyEFuncionario" element={<LoginRyEFuncionario />} />
+        <Route path="/LoginRyEAdministrador" element={<LoginRyEAdministrador />} />
+        <Route path="/PerfilFuncionario" element={<PerfilFuncionario />} />
+        <Route path="/PerfilAdministrador" element={<PerfilAdministrador />} />
+        <Route path="/ListaFuncionario" element={<ListaFuncionario />} />
+        <Route path="/funcionarios/:id" element={<SubPerfilFuncionario />} />
+        <Route path="/Estadisticas/:id?" element={<Estadisticas />} />
+        <Route path="/Probabilidades/:id?" element={<Probabilidades />} />
+        <Route path="/AnalisisRiesgo/:id?" element={<AnalisisRiesgo />} />
+        <Route path="/EstadisticasSistema/:id?" element={<EstadisticasSistema />} />
+        <Route path="/AdministradorDashboard/:id?" element={<AdministradorDashboard />} />
+        <Route path="/ListaFuncionarioRyE" element={<ListaFuncionarioRyE />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
