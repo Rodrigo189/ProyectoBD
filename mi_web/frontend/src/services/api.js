e3c37c622c146aab433da27e3f9aaeef06ada4ff
Rodrigo <107264333+Rodrigo189@users.noreply.github.com>
2025-12-01 22:52:30 -0300

    Prueba de error

--- mi_web/frontend/src/services/api.js
+++ mi_web/frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Configura la instancia de Axios apuntando a tu backend Flask
 const api = axios.create({
-  baseURL: "https://eleam.onrender.com", 
+  baseURL: "https://clinica-eleam.onrender.com", 
   headers: {
     "Content-Type": "application/json",
   },
